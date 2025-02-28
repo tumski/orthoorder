88aefd82db1856395c786fdede931c04223dfb80
Bart Zyglowicz <tumaniac@gmail.com>
2025-02-28 17:21:25 +0100

    Revert "rever"
    This reverts commit 02aee08758704a6a7d059df91633af63f38959e1.

--- src/lib/tsrpc-client.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClient } from "tsrpc-browser";
-import { serviceProto } from "../shared/protocols/serviceProto.js";
-
-// Create client
-export const client = new HttpClient(serviceProto, {
-  server: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
-  json: true,
-});
--- src/shared/protocols/Hello.ts
+++
@@ -1,8 +0,0 @@
-export interface ReqHello {
-  name?: string;
-}
-
-export interface ResHello {
-  message: string;
-  time: Date;
-}
